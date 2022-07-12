67895798deb668dffc4714201907a4988dadcbf9
NhanTruong0411 <truongnhan0411@gmail.com>
2022-07-12 14:41:11 +0700

    update dashboard page, add name

--- src/modules/article/views/sidebar.vue
+++ src/modules/article/views/sidebar.vue
@@ -1,25 +1,42 @@
 <template>
    <div class="my-dashboard">
-      <h1>Dashboard Page</h1>
+      <div class="layout">
+         <h1>Dashboard Page</h1>
+         <h2>hello, {{user.name}}</h2>
+      </div>
    </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
 export default {
+   computed: {
+      ...mapGetters({
+         user: 'auth/getUser',
+      })
 
+   }
 };
 </script>
 
 <style lang="scss" scoped>
 .my-dashboard {
-   display: flex;
-   justify-content: center;
-   align-content: center;
-   padding-top: 200px;
-
-   h1 {
-      font-size: 100px;
-      font-weight: 700;
+   .layout {
+      width: 100%;
+      height: 50vh;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      gap: 100px;
+      h1 {
+         font-size: 100px;
+         font-weight: 700;
+      }
+      h2 {
+         font-size: 80px;
+         font-weight: 500;
+      }
    }
 }
 </style>
